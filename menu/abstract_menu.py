2540a9164670f18f3d7eddbe37aa7987c4c2b0e9
kz3ko <xiixikx@gmail.com>
2021-11-28 18:12:06 +0100

    Changed indentations from 2 spaces to 4.

--- menu/abstract_menu.py
+++ menu/abstract_menu.py
@@ -1,39 +1,40 @@
-from abc import ABC, abstractclassmethod
 import curses
+from abc import ABC, abstractclassmethod
+
 
 class AbstractMenu(ABC):
-  @abstractclassmethod
-  def menuOptions(self):
-    return []
+    @abstractclassmethod
+    def menuOptions(self):
+        return []
 
-  @abstractclassmethod
-  def title(self):
-    return "Abstract Menu"
+    @abstractclassmethod
+    def title(self):
+        return "Abstract Menu"
 
-  # it should return entered submenu or None (then app should return to parent menu)
-  @abstractclassmethod
-  def handleAction(self, selectedRow):
-    return None
+    # it should return entered submenu or None (then app should return to parent menu)
+    @abstractclassmethod
+    def handleAction(self, selectedRow):
+        return None
 
-  def __init__(self, stdscr):
-    self.stdscr = stdscr
+    def __init__(self, stdscr):
+        self.stdscr = stdscr
 
-  def show(self, selectedRow):
-    self.stdscr.clear()
-    h, w = self.stdscr.getmaxyx()
-    # self.stdscr.attron(curses.color_pair(1))
+    def show(self, selectedRow):
+        self.stdscr.clear()
+        h, w = self.stdscr.getmaxyx()
+        # self.stdscr.attron(curses.color_pair(1))
 
-    selectedRowIndex = selectedRow % len(self.menuOptions())
-    self.stdscr.addstr(0, 0, self.title())
+        selectedRowIndex = selectedRow % len(self.menuOptions())
+        self.stdscr.addstr(0, 0, self.title())
 
-    for idx, item in enumerate(self.menuOptions()):
-      x = w//2 - len(item)//2
-      y = h//2 - len(self.menuOptions())//2 + idx
-      if idx == selectedRowIndex:
-        self.stdscr.attron(curses.color_pair(2))
-      self.stdscr.addstr(y, x, item)
-      if idx == selectedRowIndex:
-        self.stdscr.attroff(curses.color_pair(2))
+        for idx, item in enumerate(self.menuOptions()):
+            x = w // 2 - len(item) // 2
+            y = h // 2 - len(self.menuOptions()) // 2 + idx
+            if idx == selectedRowIndex:
+                self.stdscr.attron(curses.color_pair(2))
+            self.stdscr.addstr(y, x, item)
+            if idx == selectedRowIndex:
+                self.stdscr.attroff(curses.color_pair(2))
 
-    # self.stdscr.attroff(curses.color_pair(1))
-    self.stdscr.refresh()
+        # self.stdscr.attroff(curses.color_pair(1))
+        self.stdscr.refresh()
