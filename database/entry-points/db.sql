8973f5a2f623a70c98368f01a3f1c289b3a8cf95
Kamil Skomro <kamil.skomro@comarch.com>
2022-01-13 12:14:04 +0100

    git add db.sql file

---
+++ database/entry-points/db.sql
@@ -0,0 +1,29 @@
+CREATE TABLE detections (
+    detection_id SERIAL PRIMARY KEY,
+    detection_time TIMESTAMP NOT NULL,
+    attacker_ip_address VARCHAR(15) NOT NULL,
+    module_name VARCHAR(20) NOT NULL,
+    note VARCHAR(255)
+);
+
+CREATE TABLE dos_module_stats (
+    id SERIAL PRIMARY KEY,
+    time_window_start TIMESTAMP NOT NULL,
+    time_window_end TIMESTAMP NOT NULL,
+    mean_packets_per_addr REAL,
+    mean_packets_size_per_addr REAL
+);
+
+CREATE TABLE brute_force_module_stats (
+    id SERIAL PRIMARY KEY,
+    time_window_start TIMESTAMP NOT NULL,
+    time_window_end TIMESTAMP NOT NULL,
+    mean_tries_per_addr REAL
+);
+
+CREATE TABLE port_scanning_module_stats (
+    id SERIAL PRIMARY KEY,
+    time_window_start TIMESTAMP NOT NULL,
+    time_window_end TIMESTAMP NOT NULL,
+    mean_scans_per_addr REAL
+);
